086af4b9d8326ca599a06781a1ab2d71a0065804
Shivu <150649392+Shivukumar-M@users.noreply.github.com>
2025-07-16 20:06:16 +0530

    Update settings.py

--- djchatapp/settings.py
+++ djchatapp/settings.py
@@ -161,3 +161,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
