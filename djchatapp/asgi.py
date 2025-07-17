13951c2bb1417e0a80d3cd6104e191743ac38946
shivukumarlearn7@gmail.com <shivukumarlearn7@gmail.com>
2025-07-17 22:56:29 +0530

    added redis

--- djchatapp/asgi.py
+++ djchatapp/asgi.py
@@ -38,14 +38,12 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djchatapp.settings') 
 
+import rooms.routing
 
 # django_asgi_app = django.core.asgi.get_asgi_application()
 def get_default_application():
     from django.core.asgi import get_asgi_application
     return get_asgi_application()
-
-
-import rooms.routing
 
 
 application = ProtocolTypeRouter({
